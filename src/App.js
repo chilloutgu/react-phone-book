f421932146e0449fed1403eebc0aa4b03e9b16cd
chillgu <bbonggu92@gmail.com>
2020-05-17 11:11:41 +0900

    handle arrays in state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
-class App extends React.Component {
+class App extends Component {
+
+  id = 2;
+
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김동구',
+        phone: '010-7346-1206'
+      },
+      {
+        id: 1,
+        name: '이재혁',
+        phone: '010-8560-7007'
+      }
+    ]
+  }
 
   handleCreate = (data) => {
-    console.log(data);
+
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+
+    console.log(data)
   }
 
   render() {
+
+    const { information } = this.state;
+
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(information)}
       </div>
     )
   }
--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -5,12 +5,12 @@ class PhoneForm extends Component {
     state = {
         name: '',
         phone: ''
-    };
+    }
 
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
-        });
+        })
     }
 
     handleSubmit = (e) => {
@@ -25,8 +25,8 @@ class PhoneForm extends Component {
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input name="name" value={this.state.name} onChange={this.handleChange} placeholder="이름을 입력하세요." />
-                <input name="phone" value={this.state.phone} onChange={this.handleChange} placeholder="전화번호를 입력하세요." />
+                <input name="name" value={this.state.name} onChange={this.handleChange} placeholder="이름" />
+                <input name="phone" value={this.state.phone} onChange={this.handleChange} placeholder="전화번호" />
                 <button type="submit">등록</button>
             </form>
         )
